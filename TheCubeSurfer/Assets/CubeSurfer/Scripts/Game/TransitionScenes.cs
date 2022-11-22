d36120da10d13da6b403454e46a6739cd2004daf
Berfin <berfinackgz@gmail.com>
2022-11-22 21:20:37 +0300

    ++

--- TheCubeSurfer/Assets/CubeSurfer/Scripts/Game/TransitionScenes.cs
+++ TheCubeSurfer/Assets/CubeSurfer/Scripts/Game/TransitionScenes.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
 public class TransitionScenes : MonoBehaviour
 {
+
+
+    public RectTransform MenuUI;
+    public RectTransform InformUI;   
+    
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
     }
+
+
+    public void ActivateInformUI()
+    {
+        MenuUI.gameObject.SetActive(false);
+        InformUI.gameObject.SetActive(true);
+    }
+
+    public void DeActivateInformUI()
+    {
+        MenuUI.gameObject.SetActive(true);
+        InformUI.gameObject.SetActive(false);
+    }
+
 }
